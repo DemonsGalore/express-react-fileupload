8e7a0d659a8ea38d13fe9c1c38b2f80885a35270
Andreas Stopat <dev@demonsgalore.de>
2019-07-25 17:27:57 +0200

    Setup server and react

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No fle uploaded.' });
+  }
+
+  const { file } = req.files;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, error => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send(error);
+    }
+
+    return res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+app.listen(5000, () => console.log('Server started...'));
